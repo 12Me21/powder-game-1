2fd1d6c207d2136df090323f64419c37853a0579
12Me21 <12Me21.mc@gmail.com>
2021-02-27 20:21:05 -0500

    split platform.c
    Former-commit-id: 906e62768433ba57529174689796a4f103dcd74d

--- platform-x11.c
+++ platform-x11.c
@@ -1,10 +1,10 @@
+#include <time.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/xpm.h>
+#include <X11/keysym.h>
 #include <stdio.h>
 #include <stdlib.h>
-#ifdef _WIN32
-#include <windows.h>
-#include <dwmapi.h>
-#undef RGB
-#endif
 #include "common.h"
 #include "input.h"
 #include "save.h"
@@ -18,128 +18,6 @@ extern bool Platform_keys[256];
 
 void Platform_frame(void);
 
-#ifdef _WIN32
-// https://gist.github.com/niconii/4269d1f938ba56cac6cb44376468f7bb
-
-HWND win;
-
-long Platform_millisec(void) {
-	return GetTickCount();
-}
-
-void DrawPixels(HDC hdc) {
-	static const BITMAPINFO bmi = {
-		{
-			sizeof(BITMAPINFOHEADER),
-			WIDTH, -HEIGHT,
-			1, 32, BI_RGB, 0,
-			0, 0, 0, 0
-		},
-		{{0, 0, 0, 0}}
-	};
-	StretchDIBits(
-		hdc,
-		0, 0, WINDOW_WIDTH, WINDOW_HEIGHT,
-		8, 0, WINDOW_WIDTH, WINDOW_HEIGHT, //idk why this isn't 8,8
-		grp, &bmi,
-		DIB_RGB_COLORS,
-		SRCCOPY
-	);
-}
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-	switch(msg) {
-	case WM_PAINT:;
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
-		DrawPixels(hdc);
-		EndPaint(hwnd, &ps);
-		break;
-	case WM_CLOSE:
-		DestroyWindow(hwnd);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hwnd, msg, wParam, lParam);
-	}
-	return 0;
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	MSG msg;
-	WNDCLASSW wc = {
-		.style = CS_HREDRAW | CS_VREDRAW,
-		.lpszClassName = L"Pixels",
-		.hInstance = hInstance,
-		.hbrBackground = GetSysColorBrush(COLOR_3DFACE),
-		.lpfnWndProc = WndProc,
-		.hCursor = LoadCursor(0, IDC_ARROW),
-	};
-	RegisterClassW(&wc);
-	RECT rect = {
-		.top=0,
-		.left=0,
-		.bottom=WINDOW_HEIGHT,
-		.right=WINDOW_WIDTH,
-	};
-	DWORD style = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
-	AdjustWindowRect(&rect, style, false);
-	win = CreateWindowW(wc.lpszClassName, L"Pixels", style, CW_USEDEFAULT, CW_USEDEFAULT, rect.right-rect.left, rect.bottom-rect.top, NULL, NULL, hInstance, NULL);
-	
-	Load_test();
-
-	HDC hdc = GetDC(win);
-	float delta = 0;
-	while(1) {
-		DWORD dwStart = timeGetTime();
-		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) != 0) {
-			if (msg.message == WM_QUIT)
-				goto exit;
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-
-		POINT p;
-		GetCursorPos(&p);
-		ScreenToClient(win, &p);
-		Platform_mouseX = p.x;
-		Platform_mouseY = p.y;
-		SHORT b = GetAsyncKeyState(VK_LBUTTON);
-		Platform_mouseLeft = !!(b & 1<<15);
-		b = GetAsyncKeyState(VK_RBUTTON);
-		Platform_mouseRight = !!(b & 1<<15);
-		
-		Platform_frame();
-		
-		DrawPixels(hdc);
-
-		//DwmFlush();
-		/*delta = timeGetTime() - dwStart;
-		int amt = delta-1000.0/60;
-		//printf("%ld\n", timeGetTime() - dwStart);
-		if (amt>0) {
-			Sleep(amt);
-			//delta-=amt;
-			}*/
-	}
- exit:
-	return msg.wParam;
-}
-
-#else
-//
-// X11 flavor
-//
-//https://www.linuxquestions.org/questions/programming-9/how-to-draw-color-images-with-xlib-339366/
-#include <time.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/xpm.h>
-#include <X11/keysym.h>
-#include "common.h"
-#include "input.h"
 
 Display* D;
 Window win;
@@ -255,5 +133,3 @@ int main(int argc, char** argv) {
 		processEvent();
 	}
 }
-
-#endif
